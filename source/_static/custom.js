113422eeeabaa87cc152ba41e2fc058424372435
Cwohaocai <1450789009@qq.com>
2025-03-14 14:11:08 +0800

    en MasterPi2 commit

--- source/_static/custom.js
+++ source/_static/custom.js
@@ -1,25 +1,24 @@
 function initMenu() {
-  const menuIli = document.querySelectorAll('.toctree-l2');
-  const alreadyItems = document.querySelector('.toctree-l2.current');
-
-  if (alreadyItems) {
-    alreadyItems.classList.toggle('current');
-    alreadyItems.classList.add('h-current');
-  }
-
-  // 事件委托绑定到父级
-  document.addEventListener('click', function(e) {
-    const target = e.target.closest('.toctree-l2');
-    if (target) {
-      target.classList.toggle('h-current');
-      console.log(`元素类名已切换: ${target.className}`);
+    const alreadyItems = document.querySelector('.toctree-l2.current');
+    if (alreadyItems) {
+        alreadyItems.classList.remove('current');
+        alreadyItems.classList.add('h-current');
     }
-  });
+
+    // 事件委托绑定到父级
+    const menuContainer = document.querySelector('.wy-menu-vertical > .current');
+    menuContainer.addEventListener('click', function(e) {
+        const target = e.target.closest('.toctree-l2>a');
+        if (target) {
+            console.log(1);
+            target.parentNode.classList.toggle('h-current');
+        }
+    });
 }
 
 // 根据场景选择执行时机
 if (document.readyState === 'complete') {
-  initMenu();
+    initMenu();
 } else {
-  window.addEventListener('load', initMenu);
+    window.addEventListener('load', initMenu);
 }
